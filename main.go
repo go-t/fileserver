6e096ea7a2f1f5e34bafc278ce915b799cc2f16f
Zhusheng <zhusheng@megvii.com>
2017-11-15 14:32:28 +0800

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var (
+	flagV    = flag.Bool("v", true, "verbose")
+	flagBind = flag.String("bind", ":8000", "http listen address")
+	flagRoot = flag.String("root", ".", "document root dir")
+)
+
+func main() {
+	if *flagV {
+		fmt.Printf("run file server on address %s dir %s\n", *flagBind, *flagRoot)
+	}
+
+	var handler http.Handler = http.FileServer(http.Dir(*flagRoot))
+	if *flagV {
+		next := handler
+		handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			fmt.Printf("%s - %s %s - %v\n", r.RemoteAddr, r.Method, r.RequestURI, time.Since(start))
+		})
+	}
+
+	err := http.ListenAndServe(*flagBind, handler)
+	if err != nil {
+		panic(err)
+	}
+}
